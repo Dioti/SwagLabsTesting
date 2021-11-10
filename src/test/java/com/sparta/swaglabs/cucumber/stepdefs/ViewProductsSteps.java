815a3324fd0a6f4d046ca75b82beea978c276236
Denis-Neagu <denis.neagu@outlook.com>
2021-11-10 18:50:09 +0000

    added skeleton for products feature and step definitions

---
+++ src/test/java/com/sparta/swaglabs/cucumber/stepdefs/ViewProductsSteps.java
@@ -0,0 +1,37 @@
+package com.sparta.swaglabs.cucumber.stepdefs;
+
+import com.sparta.swaglabs.pom.util.DriverManager;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.openqa.selenium.WebDriver;
+
+public class ViewProductsSteps {
+    private WebDriver webDriver;
+
+    @Before
+    public void setUp() {
+        DriverManager.setDriverLocation("chrome", "src/test/resources/chromedriver.exe");
+    }
+
+    @Then("I should see Product Title")
+    public void iShouldSeeProductTitle() {
+    }
+
+    @And("I should see Product Description")
+    public void iShouldSeeProductDescription() {
+    }
+
+    @And("I should see Product Price")
+    public void iShouldSeeProductPrice() {
+    }
+
+    @Given("I am on the Cart page")
+    public void iAmOnTheCartPage() {
+    }
+
+    @And("I should see Product Quantity")
+    public void iShouldSeeProductQuantity() {
+    }
+}

82ac2efdfda9868b54ae8b27fa3ef46ced848ec7
Denis-Neagu <denis.neagu@outlook.com>
2021-11-10 16:25:53 +0000

    added skeleton for hamburger menu feature and step definitions

---
+++ src/test/java/com/sparta/swaglabs/cucumber/stepdefs/HamburgerMenuDefs.java
@@ -0,0 +1,60 @@
+package com.sparta.swaglabs.cucumber.stepdefs;
+
+import com.sparta.swaglabs.pom.util.DriverManager;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.WebDriver;
+
+public class HamburgerMenuDefs {
+    private WebDriver webDriver;
+
+    @Before
+    public void setUp() {
+        DriverManager.setDriverLocation("chrome", "src/test/resources/chromedriver.exe");
+    }
+
+    @When("I click on the burger menu")
+    public void iClickOnTheBurgerMenu() {
+    }
+
+    @And("I click the All Items link")
+    public void iClickTheAllItemsLink() {
+    }
+
+    @Then("I am redirected to the Products page")
+    public void iAmRedirectedToTheProductsPage() {
+    }
+
+    @And("I click the About link")
+    public void iClickTheAboutLink() {
+    }
+
+    @Then("I am redirected to saucelabs.com")
+    public void iAmRedirectedToSaucelabsCom() {
+    }
+
+    @And("I click on the log out link")
+    public void iClickOnTheLogOutLink() {
+    }
+
+    @Then("I am logged out and redirected to the log in page")
+    public void iAmLoggedOutAndRedirectedToTheLogInPage() {
+    }
+
+    @And("I click on Reset App State link")
+    public void iClickOnResetAppStateLink() {
+    }
+
+    @Then("All items in the cart are removed")
+    public void allItemsInTheCartAreRemoved() {
+    }
+
+    @After
+    public void finished() {
+        webDriver.quit();
+    }
+
+}
---
+++ src/test/resources/features/hamburgerMenu.feature
@@ -0,0 +1,23 @@
+Feature: To have a burger menu with different options to chose from
+
+  Background: I am logged in
+
+  Scenario: As a user, I want to see all products
+    When I click on the burger menu
+    And I click the All Items link
+    Then I am redirected to the Products page
+
+  Scenario: As a user, I want to view more about the company
+    When I click on the burger menu
+    And I click the About link
+    Then I am redirected to saucelabs.com
+
+  Scenario: As a user, I want to log out
+    When I click on the burger menu
+    And I click on the log out link
+    Then I am logged out and redirected to the log in page
+
+    Scenario: As a user, I want to clear the cart
+      When I click on the burger menu
+      And I click on Reset App State link
+      Then All items in the cart are removed

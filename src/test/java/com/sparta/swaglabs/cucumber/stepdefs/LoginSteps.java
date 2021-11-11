d329bd2e1814084a2f3bd43446c3d84631467648
Natasha <natasha.pearl08@gmail.com>
2021-11-11 09:11:13 +0000

    updated feature file and step definitions for login feature

--- src/test/java/com/sparta/swaglabs/cucumber/stepdefs/LoginSteps.java
+++ src/test/java/com/sparta/swaglabs/cucumber/stepdefs/LoginSteps.java
@@ -1,4 +1,48 @@
 package com.sparta.swaglabs.cucumber.stepdefs;
 
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class LoginSteps {
+    @Given("I am on the Login page")
+    public void iAmOnTheLoginPage() {
+    }
+
+    @When("I type in a valid password in the password field")
+    public void iTypeInAValidPasswordInThePasswordField() {
+    }
+
+    @And("I press the Login button")
+    public void iPressTheLoginButton() {
+    }
+
+    @Then("I should see a \"username is required\" error message")
+    public void iShouldSeeAUsernameIsRequiredErrorMessage() {
+    }
+
+    @Then("I should see a \"password is required\" error message")
+    public void iShouldSeeAPasswordIsRequiredErrorMessage() {
+    }
+
+    @When("I type in a valid username in the username field")
+    public void iTypeInAValidUsernameInTheUsernameField() {
+    }
+
+    @Then("I should login successfully")
+    public void iShouldLoginSuccessfully() {
+    }
+
+    @And("I type in an invalid password in the password field")
+    public void iTypeInAnInvalidPasswordInThePasswordField() {
+    }
+
+    @Then("I should see an error message about invalid credentials")
+    public void iShouldSeeAnErrorMessageAboutInvalidCredentials() {
+    }
+
+    @When("I type in an invalid username in the username field")
+    public void iTypeInAnInvalidUsernameInTheUsernameField() {
+    }
 }

248aa06e1cd09a82c5eb8301ce8ba39ce5c930db
Natasha <natasha.pearl08@gmail.com>
2021-11-11 10:14:52 +0000

    basic implementation of login stepdefs

--- src/test/java/com/sparta/swaglabs/cucumber/stepdefs/LoginSteps.java
+++ src/test/java/com/sparta/swaglabs/cucumber/stepdefs/LoginSteps.java
@@ -1,11 +1,39 @@
 package com.sparta.swaglabs.cucumber.stepdefs;
 
+import com.sparta.swaglabs.pom.pages.LoginPage;
+import com.sparta.swaglabs.pom.pages.ProductsPage;
+import com.sparta.swaglabs.pom.util.DriverManager;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.ProfilesIni;
 
 public class LoginSteps {
+
+    private static final int WAIT_TIME = 0;
+    private static final boolean HEADLESS = true;
+    private static final String DRIVER_LOC = "src/test/resources/geckodriver.exe";
+
+    private static WebDriver driver;
+    private static FirefoxOptions options;
+    private static LoginPage loginPage;
+
+    @Before
+    public void setup() {
+        // TODO: use factory instead
+        DriverManager.setDriverLocation("firefox", DRIVER_LOC);
+        options = new FirefoxOptions();
+        options.setProfile(new ProfilesIni().getProfile("dev"));
+        options.setHeadless(HEADLESS);
+        driver = new FirefoxDriver(options);
+    }
+
     @Given("I am on the Login page")
     public void iAmOnTheLoginPage() {
     }
@@ -40,9 +68,24 @@ public class LoginSteps {
 
     @Then("I should see an error message about invalid credentials")
     public void iShouldSeeAnErrorMessageAboutInvalidCredentials() {
+        loginPage = new LoginPage(driver);
+        loginPage.fillWithUsername("standard_user");
+        loginPage.fillWithPassword("secret_sauce");
+        loginPage.submitLogin();
+
+        //ProductsPage productsPage = new ProductsPage(driver);
+        ProductsPage productsPage = loginPage.getRedirect();
+        //productsPage.goToTwitter();
+        //System.out.println(driver.getCurrentUrl());
+        System.out.println(productsPage.goToTwitter().getUrl());
     }
 
     @When("I type in an invalid username in the username field")
     public void iTypeInAnInvalidUsernameInTheUsernameField() {
+    }
+
+    @After
+    public void teardown() {
+        driver.quit();
     }
 }

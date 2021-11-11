e5265858f174470ce3d471945f1ab025f80b21d3
unknown <lpuma@spartaglobal.com>
2021-11-11 14:31:51 +0000

    added test for viewing the cart

--- src/test/java/com/sparta/swaglabs/cucumber/stepdefs/ViewCartSteps.java
+++ src/test/java/com/sparta/swaglabs/cucumber/stepdefs/ViewCartSteps.java
@@ -1,9 +1,11 @@
 package com.sparta.swaglabs.cucumber.stepdefs;
 
 import com.sparta.swaglabs.pom.pages.CartPage;
+import com.sparta.swaglabs.pom.pages.LoginPage;
 import com.sparta.swaglabs.pom.pages.ProductsPage;
 import com.sparta.swaglabs.pom.util.DriverManager;
 import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.jupiter.api.Assertions;
@@ -11,19 +13,23 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ViewCartSteps {
-    /*private WebDriver webDriver;
+    private WebDriver webDriver;
     private ProductsPage productsPage;
-    private CartPage cartPage;
+    //private CartPage cartPage;
+    private LoginPage loginPage;
+
     @Before
     public void setUp() {
         DriverManager.setDriverLocation("chrome", "src/test/resources/chromedriver.exe");
         webDriver=new ChromeDriver();
-        productsPage=new ProductsPage(webDriver);
-        cartPage=new CartPage(webDriver);
+        loginPage= new LoginPage(webDriver);
+        loginPage.login("standard_user","secret_sauce");
+        productsPage=loginPage.getRedirect();
     }
 
     @When("I click on the cart icon")
     public void iClickOnTheCartIcon() {
+
         productsPage.goToCart();
     }
 
@@ -31,5 +37,15 @@ public class ViewCartSteps {
     public void iWantToSeeTheCorrectProductsInMyShoppingCart() {
         String actualUrl=webDriver.getCurrentUrl();
         Assertions.assertEquals("https://www.saucedemo.com/cart.html",actualUrl);
-    }*/
+    }
+
+    @When("I am on the cart page")
+    public void iAmOnTheCartPage() {
+        productsPage.goToCart();
+    }
+
+    @Then("I want to see the details of the products in my shopping cart")
+    public void iWantToSeeTheDetailsOfTheProductsInMyShoppingCart() {
+        
+    }
 }

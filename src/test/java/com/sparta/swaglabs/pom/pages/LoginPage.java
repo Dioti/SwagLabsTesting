6974a355448e4a0675f737d33286b6e48dd10c13
Natasha <natasha.pearl08@gmail.com>
2021-11-11 10:18:38 +0000

    updated constructor for loginpage

--- src/test/java/com/sparta/swaglabs/pom/pages/LoginPage.java
+++ src/test/java/com/sparta/swaglabs/pom/pages/LoginPage.java
@@ -1,16 +1,33 @@
 package com.sparta.swaglabs.pom.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class LoginPage extends Page {
 
     public LoginPage(WebDriver driver) {
-        this.driver = driver;
+        super(driver);
         goToLoginPage();
     }
 
     private void goToLoginPage() {
         driver.get("https://www.saucedemo.com/");
+    }
+
+    public void fillWithUsername(String user) {
+        driver.findElement(By.id("user-name")).sendKeys(user);
+    }
+
+    public void fillWithPassword(String pass) {
+        driver.findElement(By.id("password")).sendKeys(pass);
+    }
+
+    public void submitLogin() {
+        driver.findElement(By.id("login-button")).click();
+    }
+
+    public ProductsPage getRedirect() {
+        return new ProductsPage(driver);
     }
 
 }
